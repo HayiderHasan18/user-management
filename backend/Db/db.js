cc581c8bccd79210155ea8d851f96db8b05ead1c
HayiderHasan18 <hayiderhasan26@gmail.com>
2025-12-10 19:42:38 +0300

    modified

--- backend/Db/db.js
+++ backend/Db/db.js
@@ -1,20 +1,14 @@
-const mysql2 = require('mysql2')
-const dbConnection = mysql2.createPool({
-	user: process.env.USER,
-	host: "localhost",
-	password: process.env.PASSWORD,
-	database: process.env.DATABASE,
-	connectionLimit: 10
+const mysql2 = require('mysql2');
 
-})
-console.log(process.env.JWT_SECRET)
-// dbConnection.execute(" select 'select' ",(err, result)=> {
-// 	if (err)
-// 	{
-// 		console.log(err.message)
-// 	} else
-// 	{
-// 		console.log(result)
-// 	}
-// })
-module.exports = dbConnection.promise()
+const dbConnection = mysql2.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10
+});
+
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
+module.exports = dbConnection.promise();
